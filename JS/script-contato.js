ffebdd65d07d2fe5af4ed6d5aa646217ee4b1a08
Henrique Anzilago <88406845+Anzilago@users.noreply.github.com>
2023-05-11 21:40:20 -0400

    Add files via upload

--- JS/script-contato.js
+++ JS/script-contato.js
@@ -1,3 +1,4 @@
+
 function enviar(){
     const nome = document.querySelector("#nome").value;
     const telefone = document.querySelector("#telefone").value;
@@ -9,3 +10,27 @@ function enviar(){
         return alert("Dados enviados!\n" + "Em breve entraremos em contato\n" + "Até logo")        
     }
 }
+
+
+function consultaMarca(){
+
+    let url = `https://parallelum.com.br/fipe/api/v1/carros/marcas`;
+    fetch(url).then(function (response){
+        console.log(response);
+        response.json().then(function (data){
+            let divs = document.querySelector(".containerMarcas");
+            for (let i = 0; i < (data.length/2)-2; i++) {
+                divs.innerHTML += `<div>${data[i].nome}</div>`;
+            }
+        })
+    })
+}
+
+
+
+
+
+
+
+
+
